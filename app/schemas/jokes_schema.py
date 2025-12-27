8a80ac1264f0cd7e3fe1ab090401ab679e5b10fd
RomesAll <romesky845@gmail.com>
2025-12-27 19:55:52 +0300

    добавил pydantic схемы

---
+++ app/schemas/jokes_schema.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, ConfigDict, Field
+from datetime import datetime
+
+class JokesSchemaPOST(BaseModel):
+    text: str = Field(default='def_text', min_length=10, max_length=100, examples=['ha-ha-ha'], description='Текст шутки')
+    count_likes: int = Field(default='def_count_likes', ge=0, le=1000, examples=['0'], description='Кол-во лайков')
+    count_dislikes: int = Field(default='def_count_dislikes', ge=0, le=1000, examples=['0'], description='Кол-во дизлайков')
+
+class JokesSchemaGET(JokesSchemaPOST):
+    id: int
+    created_at: datetime
+    updated_ad: datetime
+
+class JokesSchemaPUT(JokesSchemaPOST):
+    id: int
