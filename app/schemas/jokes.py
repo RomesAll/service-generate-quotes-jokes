b63529e0abd6211d3aa0f2e3879c817b70f5e46c
RomesAll <romesky845@gmail.com>
2025-12-28 14:43:07 +0300

    добавил сервисы, исправил ошибку в схеме quotes

--- app/schemas/jokes.py
+++ app/schemas/jokes.py
@@ -5,6 +5,7 @@ class JokesSchemaPOST(BaseModel):
     text: str = Field(default='def_text', min_length=10, max_length=100, examples=['ha-ha-ha'], description='Текст шутки')
     count_likes: int = Field(default='def_count_likes', ge=0, le=1000, examples=['0'], description='Кол-во лайков')
     count_dislikes: int = Field(default='def_count_dislikes', ge=0, le=1000, examples=['0'], description='Кол-во дизлайков')
+    model_config = ConfigDict(from_attributes=True)
 
 class JokesSchemaGET(JokesSchemaPOST):
     id: int
--- app/schemas/quotes.py
+++ app/schemas/quotes.py
@@ -1,20 +1,33 @@
 from pydantic import BaseModel, ConfigDict, Field, computed_field
 from datetime import datetime
 
-class JokesSchemaPOST(BaseModel):
-    text: str = Field(default='def_text')
-    count_likes: int = Field(default='def_count_likes')
-    count_dislikes: int = Field(default='def_count_dislikes')
+class QuotesSchemaPOST(BaseModel):
+    text: str = Field(default='def_text', min_length=10, max_length=100, examples=['ha-ha-ha'], description='Текст цитаты')
+    author_id: int
+    count_likes: int = Field(default='def_count_likes', ge=0, le=1000, examples=['0'], description='Кол-во лайков')
+    count_dislikes: int = Field(default='def_count_dislikes', ge=0, le=1000, examples=['0'], description='Кол-во дизлайков')
     model_config = ConfigDict(from_attributes=True)
 
     @computed_field
     def overall_assessment(self) -> int:
         return self.count_likes - self.count_dislikes
 
-class JokesSchemaGET(JokesSchemaPOST):
+class QuotesSchemaGET(QuotesSchemaPOST):
     id: int
     created_at: datetime
     updated_ad: datetime
 
-class JokesSchemaPUT(JokesSchemaPOST):
+class QuotesSchemaPUT(QuotesSchemaPOST):
+    id: int
+
+class AuthorSchemaPOST(BaseModel):
+    fio: str = Field(default='def_fio', min_length=5, max_length=100, examples=['Петров В.В.'])
+    model_config = ConfigDict(from_attributes=True)
+
+class AuthorSchemaGET(AuthorSchemaPOST):
+    id: int
+    created_at: datetime
+    updated_ad: datetime
+
+class AuthorSchemaPUT(AuthorSchemaPOST):
     id: int
