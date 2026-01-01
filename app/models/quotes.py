799c880424092cf5b8a9a80861cc0a0e1a642a79
RomesAll <romesky845@gmail.com>
2026-01-01 14:08:50 +0300

    добавил поле year в таблицу quotes_orm и в схеме для фильтрации

--- app/models/quotes.py
+++ app/models/quotes.py
@@ -10,6 +10,7 @@ class AuthorOrm(Base):
 class QuotesOrm(Base):
     __tablename__ = 'quotes_orm'
     text: Mapped[str] = mapped_column(unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(nullable=False)
     author_id: Mapped[int] = mapped_column(ForeignKey('author_orm.id', ondelete='CASCADE'), nullable=True)
     count_likes: Mapped[int] = mapped_column(default=0, nullable=False)
     count_dislikes: Mapped[int] = mapped_column(default=0, nullable=False)
--- app/schemas/quotes.py
+++ app/schemas/quotes.py
@@ -3,14 +3,11 @@ from datetime import datetime
 
 class QuotesSchemaPOST(BaseModel):
     text: str = Field(default='def_text', min_length=5, max_length=100, examples=['ha-ha-ha'], description='Текст цитаты')
+    year: int = Field(default='def_count_likes', ge=0, le=2050, examples=['2001'], description='Год написания шутки')
     author_id: int
     count_likes: int = Field(default='def_count_likes', ge=0, le=1000, examples=['0'], description='Кол-во лайков')
     count_dislikes: int = Field(default='def_count_dislikes', ge=0, le=1000, examples=['0'], description='Кол-во дизлайков')
     model_config = ConfigDict(from_attributes=True)
-
-    @computed_field
-    def overall_assessment(self) -> int:
-        return self.count_likes - self.count_dislikes
 
 class QuotesSchemaGET(QuotesSchemaPOST):
     id: int
